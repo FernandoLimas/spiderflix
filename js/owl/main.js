99a6c39194b5963fb9a011c59e5188427448528f
FernandoLimas <nando_filipe@hotmail.com>
2022-01-03 19:21:25 -0300

    spiderFlix

--- js/owl/main.js
+++ js/owl/main.js
@@ -1,16 +1,18 @@
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:3
-      },
-      1000:{
-          items:5
-      }
-  }
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:5,
+    loop:true,
+    margin:10,
+    nav:true,
+    navText:['previous', 'next'],
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplaySpeed:2000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
 })
